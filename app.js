fc979dd40e2726d3a2b9dc7842017aeb89bf8b53
Jeremy <jeremy.h.lee7@gmail.com>
2017-07-20 15:38:57 -0700

    skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// Set where we serve static files
+app.use(express.static(__dirname + "/public"));
+// Set where we find views
+app.set("views", __dirname + "/views" );
+
+// Manually setting a route
+app.get("/", function(request, response) {
+  response.send("Hello World!");
+});
+// get "/" do
+//   "Hello World!"
+// end
+
+// Having defined the routes in a separate file
+const test = require("./routes/test");
+app.use("/test", test);
+
+// Start the server
+app.listen(3000);
